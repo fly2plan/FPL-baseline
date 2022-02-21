ce2d540e263e3087351d1abc03598aafd5916408
harrymknight <harry.knight@hotmail.co.uk>
2022-02-21 17:34:56 +0000

    remove test code

--- client/client.py
+++ client/client.py
@@ -7,21 +7,6 @@ from zokrates_pycrypto.eddsa import PrivateKey, PublicKey
 from zokrates_pycrypto.field import FQ
 from zokrates_pycrypto.babyjubjub import JUBJUB_L
 
-# raw_msg = "This is my secret message"
-# msg = hashlib.sha512(raw_msg.encode("utf-8")).digest()
-
-# Seeded for debug purpose
-# key = FQ(1997011358982923168928344992199991480689546837621580239342656433234255379025)
-# sk = PrivateKey(key)
-# sk = PrivateKey.from_rand()
-# sig = sk.sign(msg)
-
-# pk = PublicKey.from_private(sk)
-# is_verified = pk.verify(sig, msg)
-# print(is_verified)
-
-# path = 'zokrates_inputs.txt'
-# write_signature_for_zokrates_cli(pk, sig, msg, path)
 
 
 def get_rand_n():
